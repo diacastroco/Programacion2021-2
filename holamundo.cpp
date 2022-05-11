4fd119036bc4dd07e6993a2cfaac5a50e4d99faa
diacastroco <diacastroco@unal.edu.co>
2022-05-11 03:03:36 -0500

    Programas holamundo (anotaciones) y variables Clase 3

---
+++ holamundo.cpp
@@ -0,0 +1,21 @@
+#include <iostream> //InputOutputSTREAM
+
+int main(void) //Funcion llamada "main" que retorna un entero
+{
+  std::cout << "Hola mundo con endl" << std::endl; //Se entró en la bolsa std para usar cout y endl. endl=end line
+  std::cout << "Hola mundo con /n \n"; //Poner punto a parte con \n
+  return 0; 
+}
+
+//-------------------Anotaciones del curso------------------------
+
+//Edición: Escribir mi programa con un editor
+//Compilación: g++ holamundo.cpp (cumple las reglas del lenguaje)
+//Ejecución: ./a.out
+
+
+//Variables: string,bool,int,float,double,char
+//Operadores: +,-,*,/
+//Funciones
+//Estructuras de datos básicas
+//Clases
---
+++ variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+  int a=3;
+  float x=3.2;
+  double y=0;
+  std::cout << "a = " << a << "\n"; //Los << siguen la cadena del print
+  std::cout << "x = " << x << "\n";
+  std::cout << "y = " << y << "\n"; 
+  
+}
