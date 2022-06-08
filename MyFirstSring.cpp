8f8497fb1db662d7f3e76b7a556657033325b7fb
diacastroco <diacastroco@unal.edu.co>
2022-06-08 18:50:12 -0500

    Programa String. Pendiente programa almacenamiento de memoria. Clase 4

---
+++ MyFirstSring.cpp
@@ -0,0 +1,11 @@
+//My First String
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+  std::string mystring;
+  mystring = "This is a string";
+  std::cout << mystring << std::endl;
+  return 0;
+}
